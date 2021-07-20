16b27fcc76f86d4df282f70839db862b6315c395
Yudai Nakajima <kituneudon97@gmail.com>
2021-07-20 18:29:13 +0900

    簡単なカウンター機能を追加

---
+++ src/Counter.tsx
@@ -0,0 +1,28 @@
+import { FC, useState } from "react";
+
+const Counter: FC = () => {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  const handleReset = () => {
+    setCount(0);
+  };
+
+  return (
+    <div>
+      <span>{count}</span>
+      <button onClick={increment}>+</button>
+      <button onClick={decrement}>-</button>
+      <button onClick={handleReset}>リセット</button>
+    </div>
+  );
+};
+
+export default Counter;
