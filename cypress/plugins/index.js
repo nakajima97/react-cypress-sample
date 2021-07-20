64b940adbfdbe6861eff08f2c688a230b3079bec
Yudai Nakajima <kituneudon97@gmail.com>
2021-07-20 18:17:00 +0900

    テストカバレッジを表示する

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,3 +1,5 @@
+import task from "@cypress/code-coverage/task";
+
 /// <reference types="cypress" />
 // ***********************************************************
 // This example plugins/index.js can be used to load plugins
@@ -19,4 +21,7 @@
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
-}
+  task(on, config);
+
+  return config;
+};
